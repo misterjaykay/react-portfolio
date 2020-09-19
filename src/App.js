ba6efd0ad413ba2f38ba1ec69b6f073c6a2f84fb
Jonghyun Kang <misterjaykay@gmail.com>
2020-09-18 21:08:32 -0400

    pages componenets created

--- src/App.js
+++ src/App.js
@@ -1,7 +1,24 @@
 import React from "react";
+import { BrowserRouter as Router, Route } from 'react-route-dom';
+import About from "./pages/About";
+import Contact from "./pages/Contact";
+import Portfolio from "./pages/Portfolio";
+import Project from "./pages/Project";
+import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
 
 function App() {
-  return null;
+  return (
+    <Router>
+      <Navbar />
+        <Route exact path="/" component={About} />
+        <Route exact path="/about" component={About} />
+        <Route exact path="/portfolio" component={Portfolio} />
+        <Route exact path="/project" component={Project} />
+        <Route exact path="/contact" component={Contact} />
+      <Footer />
+    </Router>
+  )
 }
 
 export default App;
