a8a2827c255d943d2c8b6bbeb02213f31ce6756b
Jonghyun Kang <misterjaykay@gmail.com>
2020-09-23 22:07:08 -0400

    Updates

---
+++ precache-manifest.ee062d48eea942e793f17be8990cb3e9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "96230a13f4d04f27d79b6dea1986b320",
+    "url": "/react-portfolio/index.html"
+  },
+  {
+    "revision": "268839192aff57263a29",
+    "url": "/react-portfolio/static/js/2.9db947f7.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/react-portfolio/static/js/2.9db947f7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ad001adacd302cfe6493",
+    "url": "/react-portfolio/static/js/main.584a989b.chunk.js"
+  },
+  {
+    "revision": "aff33cef9dbb8787d6b3",
+    "url": "/react-portfolio/static/js/runtime-main.213b1880.js"
+  }
+]);
